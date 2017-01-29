79df97a3b17eb528be375c8fc3e10343f2f89b4c
hoangtu1555 <lov3.4ver@gmail.com>
2017-01-30 00:44:31 +0700

    update skycode

---
+++ js/skycode.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('textarea').each(function(i, area) {
+        $(area).on('change keyup paste blur focus', function() {
+            var enc = $(area).attr("data-id");
+            var decode = ($(area).attr("data-type") === "encode") ? 0 : 1
+            var S = $(area).val();
+
+            doEncoding(S, enc, decode, function(ret) {
+                $("#" + enc + "_output").text(ret);
+            });
+        })
+    });
+});
+
+function doEncoding(S, encType, decode, callback) {
+    var ret = "Error";
+    if (encType === "Skycode") {
+        ret = "";
+        if (decode) {
+            S = S.replace(/'s /g," ");
+            S = S.replace(/'s/g,"");         
+            ret = S;
+        } else {
+        S = S.replace(/ /g,"'s "); if ( S.length != " " ) { S=S+"'s"};
+            ret = S;
+        }
+    }
+    console.log(ret);
+    callback(ret);
+}
